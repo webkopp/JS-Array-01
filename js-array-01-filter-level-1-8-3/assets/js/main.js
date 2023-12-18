2b9e3581d032c52db4fbf4423152cca2c1078ae7
webkopp <nakopps@googlemail.com>
2023-12-18 19:49:53 +0100

    filter-1-8-3und1-8-4

--- js-array-01-filter-level-1-8-3/assets/js/main.js
+++ js-array-01-filter-level-1-8-3/assets/js/main.js
@@ -6,3 +6,13 @@
 
 // Zur besseren Darstellung haben wir uns das array vor dem Schreiben der Funktion ausgeben lassen, und dann das array nochmal. Schau dir die Ergebnisvorschau an.
          
+const heroArr = ["Superman", "Batman", undefined, , "Wonder Woman", "Spider-Man", "Black Widow", "Iron Man", "Thor", "Catwoman", , null]
+console.log(heroArr)
+
+const myHeroes = () => {
+    let filterHero = heroArr.filter(el => el != undefined && el != null && el != 'empty')
+
+    console.log(filterHero);
+}
+
+myHeroes()
--- js-array-01-filter-level-1-8-4/assets/js/main.js
+++ js-array-01-filter-level-1-8-4/assets/js/main.js
@@ -10,21 +10,9 @@
 // Verwende die filter()-Methode, um die Zahlen kleiner als 20 zu filtern und in einem neuen Array zu speichern. 
 // Verwende die map()-Methode, um die gefilterten Zahlen mit 2 zu multiplizieren.
          
-let person = ["nancy", "kopp", "leipzig"]
-console.log(person)
-person.push("travelLover", "foodLover")
-console.log(person)
+const myNumbers = [12, 25, 7, 18, 30, 5]
+const smallThenTwenty = myNumbers.filter(el => el < 20)
+console.log(smallThenTwenty)
 
-
-let friends = ["mickey", "michelle", "maurice"]
-console.log(friends)
-friends.push("Drine", "Sebastian")
-console.log(friends)
-
-let favoriteFood = ["iceCream", "chickenSoup", "uschisNoodles"]
-console.log(favoriteFood)
-favoriteFood.push("Suhsi", "Pizza")
-console.log(favoriteFood)
-
-// let kopps = [person, friends, favoriteFood]
-// console.log(kopps)
+const multipli2 = smallThenTwenty.map(el => el * 2)
+console.log(multipli2)
